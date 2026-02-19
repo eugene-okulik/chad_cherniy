1a6e7681d3f8b8fc2ca356d2f55e2a3c688a01e7
Fortius <chadcherniy@yandex.ru>
2026-02-19 14:21:07 +0300

    updated2

--- homework/egor_fortius/Homework_15/database_lesson.py
+++ homework/egor_fortius/Homework_15/database_lesson.py
@@ -9,15 +9,20 @@ db = mysql.connect(
     database='st-onl'
 )
 
+"""Запрос 1"""
 # Для БД создается переменная cursor, через которое происходит управление
 cursor = db.cursor(dictionary=True)    # dictionary=True - получаем данные в виде словаря
-# cursor.execute('SELECT * FROM students ORDER BY id DESC LIMIT 10')   # Выполняем простой запрос
-# result = cursor.fetchall()   # Возвращает то, что выполнил запрос, в формате списка
-# for student in result:
-#     print(student['second_name'])
+cursor.execute('SELECT * FROM students ORDER BY id DESC LIMIT 10')   # Выполняем простой запрос
+result = cursor.fetchall()   # Возвращает то, что выполнил запрос, в формате списка
+for student in result:
+    print(student['second_name'])
 
+"""Запрос 2"""
 cursor.execute('SELECT * FROM students WHERE id=22284')
 result2 = cursor.fetchone()    # Если мы знаем, что результат поиска - единственный
 print(result2)
+
+"""Запрос на sql-инъекцию"""
+
 
 db.close()   # обязательно отключаться от БД
