eeabba815674179999ad124b302ca185e1320445
Egor <leo100996@gmail.com>
2026-04-13 17:41:40 +0300

    create analyzer.py

---
+++ homework/egor_fortius/python-project/analyzer.py
@@ -0,0 +1,19 @@
+import argparse
+import os
+
+
+# В программе обязательным аргументом будет имя файла, в котором будет происходить поиск
+parser = argparse.ArgumentParser()
+parser.add_argument("folder", help="Path to folder")
+parser.add_argument("-t", "--text", required=True, help="Text for search in file")
+parser.add_argument("-d", "--date", help="date for search")
+parser.add_argument("--full", help="full name.log", action="store_true")
+args = parser.parse_args()
+
+print(args.folder, args.text, args.date, args.full)
+
+# Проверяем, что папка существует
+if not os.path.exists(args.folder):
+    print("Folder not found")
+    exit(1)
+
