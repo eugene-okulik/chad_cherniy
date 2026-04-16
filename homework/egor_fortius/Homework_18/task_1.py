877a731a0c19580ae14b12c85ae85fc72a382a2f
Egor <leo100996@gmail.com>
2026-04-16 13:17:37 +0300

    final version

--- homework/egor_fortius/Homework_18/task_1.py
+++ homework/egor_fortius/Homework_18/task_1.py
@@ -9,4 +9,68 @@ def all_objects():
     data = response.json()
     ic(data)
 
-result = all_objects()
+# POST /object - создание объекта
+def create_object():
+    body = {
+        "name": "Genrih",
+        "data": {"second_name": "VIII", "age": 32}
+        }
+    response = requests.post(
+        f"{MAIN_URL}/object", timeout=10,
+        json=body,
+    )
+    assert response.status_code == 200, 'Not Success'
+    result = response.json()
+    ic(result)
+    return result
+
+# PUT /object/<id> — полное обновление
+def put_obj(obj_id):
+    body = {
+        "name": "Genrih XII",
+        "data": {"second_name": "Perdu", "age": 32}
+        }
+    response = requests.put(
+        f"{MAIN_URL}/object/{obj_id}",
+        json=body,
+    )
+    assert response.status_code == 200, 'Not Success'
+    data = response.json()
+    assert data["id"] == obj_id
+    ic(data)
+
+# PATCH /object/<id> — частичное обновление
+def update_object_patch(obj_id):
+    body = {"name": "Gondurasina"}
+    response = requests.patch(f"{MAIN_URL}/object/{obj_id}", json=body, timeout=10)
+    assert response.status_code == 200
+    result = response.json()
+    ic(result)
+    return result
+
+# DELETE /object/<id> — удаление
+def delete_object(obj_id):
+    response = requests.delete(f"{MAIN_URL}/object/{obj_id}")
+    assert response.status_code == 204
+    ic(f"✅ Объект {obj_id} удалён (204)")
+    return True
+
+# ЗАПУСК СЦЕНАРИЯ
+if __name__ == "__main__":
+    ic("Начинаем тестирование API")
+
+    # 1. Создаём объект и сразу получаем его ID
+    new_obj = create_object()
+    obj_id = new_obj["id"]
+    ic(f"Создан объект с ID: {obj_id}")
+
+    # 2. 
+    put_obj(obj_id)
+    update_object_patch(obj_id)
+    delete_object(obj_id)
+
+    # 3. Проверяем, что объект действительно удалён
+    response = requests.get(f"{MAIN_URL}/object/{obj_id}", timeout=10)
+    assert response.status_code == 404, "Объект должен быть удалён"
+
+    ic("Все тесты прошли успешно!")
