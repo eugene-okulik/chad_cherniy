448ff0da9d2dc954d855133cee83cc997e0dd441
Egor <leo100996@gmail.com>
2026-05-14 11:57:33 +0300

    edited locustfile

--- test_api_efortius/locustfile.py
+++ test_api_efortius/locustfile.py
@@ -9,7 +9,6 @@ class LocustUser(HttpUser):
     def get_all_posts(self):
         self.client.get("/object")
 
-
     @task(weight=3)
     def get_one_post(self):
         self.client.get(f"/object/{self.one_id}")
