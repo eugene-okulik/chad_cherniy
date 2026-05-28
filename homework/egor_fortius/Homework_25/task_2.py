6c2ff660fd72a917bb9feed25d868df3c63faff5
Chadcherniy <leo100996@gmail.com>
2026-05-28 11:46:56 +0300

    delete time

--- homework/egor_fortius/Homework_25/task_2.py
+++ homework/egor_fortius/Homework_25/task_2.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-import time
 
 
 options = Options()
